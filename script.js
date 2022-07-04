059b2511ffa492de512fc95dda0cece52bb8eb36
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-07-04 07:27:52 +0300

    adding event listner

--- script.js
+++ script.js
@@ -7,43 +7,43 @@ let xhr = new XMLHttpRequest()
 xhr.open('GET', 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
 xhr.responseType = 'text';
 
-// xhr.addEventListener('load', function() {
-//     if (xhr.status === 200) {
-//         p.textContent = "loading...";
-//         weatherData = JSON.parse(xhr.responseText)
-//         weatherPost()
+xhr.addEventListener('load', function() {
+    if (xhr.status === 200) {
+        p.textContent = "loading...";
+        weatherData = JSON.parse(xhr.responseText)
+        weatherPost()
 
-//     } else {
+    } else {
 
-//         p.textContent = "error: " + xhr.status
-//     }
+        p.textContent = "error: " + xhr.status
+    }
 
-// }, false)
+}, false)
 
-// xhr.send()
+xhr.send()
 
-// function weatherPost() {
-//     // time,
-//     // temperature_2m,
-//     // humidity_2m,
-//     // wind_2m,
-//     // cloud cover_2m
+function weatherPost() {
+    // time,
+    // temperature_2m,
+    // humidity_2m,
+    // wind_2m,
+    // cloud cover_2m
 
-//     const time = weatherData.hourly.time
-//     const temp = weatherData.hourly.temperature_2m
-//     const sec = weatherData.generationtime_ms
-//     const tempe = weatherData.elevation
-
-
-//     const humidity = weatherData.hourly.humidity_2m
-//     const wind = weatherData.hourly.wind_2m
-//     const cloud = weatherData.hourly.cloudcover_mid
+    const time = weatherData.hourly.time
+    const temp = weatherData.hourly.temperature_2m
+    const sec = weatherData.generationtime_ms
+    const tempe = weatherData.elevation
 
 
+    const humidity = weatherData.hourly.humidity_2m
+    const wind = weatherData.hourly.wind_2m
+    const cloud = weatherData.hourly.cloudcover_mid
 
 
-//     const str = ` Tempe:${tempe} sec:${sec} time:${time} temp:${temp} humidity:${humidity} wind:${wind} cloud:${cloud}`
-//     p.textContent = str
 
 
-// }
+    const str = ` Tempe:${tempe} sec:${sec} time:${time} temp:${temp} humidity:${humidity} wind:${wind} cloud:${cloud}`
+    p.textContent = str
+
+
+}
